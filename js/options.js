c751d5b28ab5cbaf04124cd5c39138e4a38a563d
Michael Westrich <westrich.michael@googlemail.com>
2010-03-14 22:48:52 +0000

    options page finally working.

--- js/options.js
+++ js/options.js
@@ -1,6 +1,10 @@
 // Scripts for the options.html
 
 //------------------------------------------------------------------------------
+// Global variables.
+var bgPage = chrome.extension.getBackgroundPage();
+
+//------------------------------------------------------------------------------
 // Saves options to localStorage.
 function save_options() {
   var select_tabsCount = document.getElementById("tabsCount");
@@ -29,4 +33,105 @@ function restore_options() {
       break;
     }
   }
+}
+
+//------------------------------------------------------------------------------
+//Creates list with recently closed tabs.
+function getListOfRecentlyClosedTabsComplete() {
+	var recentlyClosedTabsArray = bgPage.recentlyClosedTabs;
+	console.log(recentlyClosedTabsArray);
+
+	var rootDivElement = document.getElementById('rootDiv');
+	
+	if (recentlyClosedTabsArray.length == 0) {
+		rootDivElement.appendChild(document.createTextNode('No recently closed tabs.'));
+	} else {
+		var size = recentlyClosedTabsArray.length;
+		var tabsCount = localStorage.getItem('tabsCount');
+		var tableElement = document.createElement('table');
+		tableElement.setAttribute('id', 'table');	
+		for (var key = size - 1; key >= 0; key--) {
+			var favicon = recentlyClosedTabsArray[key].favicon;
+			var tabShot = recentlyClosedTabsArray[key].tabShot;
+			var trElement = document.createElement('tr');
+			trElement.setAttribute('id', 'tab' + key);
+			var tdTabShotElement = document.createElement('td');
+			tdTabShotElement.setAttribute('class', 'tabShotTD');
+			var tabShotIMG = document.createElement('img');
+			tabShotIMG.setAttribute('id', 'tabShot' + key);
+			tabShotIMG.setAttribute('class', 'tabShotIMG');
+			tdTabShotElement.appendChild(tabShotIMG);
+			trElement.appendChild(tdTabShotElement);
+			
+			var tdUrlElement = document.createElement('td');
+			tdUrlElement.setAttribute('class', 'urlTD');
+			var linkElement = document.createElement('a');
+			//linkElement.setAttribute('href', 'javascript:chrome.tabs.create({url: \'' + recentlyClosedTabsArray[key].url + '\'});bgPage.deleteRecentlyClosedTabById(\'' + key + '\');');
+			linkElement.setAttribute('href', 'javascript:createRecentlyClosedTab(' + key + ');');
+			linkElement.setAttribute('title', recentlyClosedTabsArray[key].url);
+			var linkText = document.createTextNode(bgPage.getTitel(recentlyClosedTabsArray[key].title));
+			linkElement.appendChild(linkText);
+			tdUrlElement.appendChild(linkElement);
+			trElement.appendChild(tdUrlElement);
+			
+			var tdFavIconElement = document.createElement('td');
+			tdFavIconElement.setAttribute('class', 'faviconTD');
+			
+			var linkElement = document.createElement('a');
+			//linkElement.setAttribute('href', 'javascript:chrome.tabs.create({url: \'' + recentlyClosedTabsArray[key].url + '\'});bgPage.deleteRecentlyClosedTabById(\'' + key + '\');');
+			linkElement.setAttribute('href', 'javascript:createRecentlyClosedTab(' + key + ');');
+			linkElement.setAttribute('title', recentlyClosedTabsArray[key].url);
+			var favIconIMG = document.createElement('img');
+			favIconIMG.setAttribute('id', 'favIcon' + key);
+			favIconIMG.setAttribute('class', 'faviconIMG');
+			if (favicon !== undefined) {
+				favIconIMG.setAttribute('src', favicon);
+			} else {
+				favIconIMG.setAttribute('src', '../images/default_favicon.png');
+			}
+			linkElement.appendChild(favIconIMG);
+			tdFavIconElement.appendChild(linkElement);
+			
+			trElement.appendChild(tdFavIconElement);
+			
+			var tdDeleteElement = document.createElement('td');
+			tdDeleteElement.setAttribute('class', 'tdDelete');
+			
+			var input = document.createElement('input');
+			input.setAttribute('type', 'button');
+			input.setAttribute('onclick', 'javascript:deleteRecentlyClosedTab(' + key + ');');
+			input.setAttribute('value', 'Delete Element');
+			
+			tdDeleteElement.appendChild(input);
+			
+			trElement.appendChild(tdDeleteElement);
+			
+			tableElement.appendChild(trElement);
+		}
+		rootDivElement.appendChild(tableElement);
+		for ( var keyImage = size - 1; keyImage >= 0; keyImage--) {
+			var tabShotImageData = recentlyClosedTabsArray[keyImage].tabShot;
+			if (tabShotImageData !== undefined && tabShotImageData != null) {
+				document.getElementById('tabShot' + keyImage).src = tabShotImageData;
+			} else {
+				document.getElementById('tabShot' + keyImage).src = '../images/default_tabShot.png';
+			}
+		}
+	}
+}
+
+//------------------------------------------------------------------------------
+//Creates recently closed tab.
+function createRecentlyClosedTab(key) {
+	chrome.tabs.create({url: bgPage.recentlyClosedTabs[key].url});
+	deleteRecentlyClosedTab(key);
+}
+
+//------------------------------------------------------------------------------
+//Deletes element from the list of recently closed tabs.
+function deleteRecentlyClosedTab(key) {
+	bgPage.deleteRecentlyClosedTabById(key);
+
+	//should remove this table row
+	document.getElementById("table").removeChild(document.getElementById('tab'+key));
 }
