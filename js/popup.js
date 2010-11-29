52b4842d878de1cdfd79607599a7eb7f71e73020
Michael Westrich <westrich.michael@googlemail.com>
2010-11-29 01:12:48 +0000

    Added own default favIcon

--- js/popup.js
+++ js/popup.js
@@ -3,52 +3,56 @@ function main() {
   bgPage.loadValues();
   
   if (bgPage.isEmpty(bgPage.rctTimestamps)) {
-	var textDiv = $('<div>').text('No recently closed tabs.').appendTo('body');
+    var textDiv = $('<div>').text('No recently closed tabs.').appendTo('body');
   } else {  
 	  var ul = $('<ul>');
 	  var counter = 0;  
 	  
 	  for (var index in bgPage.rctTimestamps) {
-		counter++;
-		if (counter > bgPage.maxPopupLength) {
-		  break;
-		}
-		var timestamp = bgPage.rctTimestamps[index];
-		var tab = bgPage.rcts[timestamp];
-		var li = $('<li>');
-		  
-		var url = $('<a>')
-		  .attr({ 
-			href: '#',
-			title: tab.url
-		  })
-		  /*.text(tab.title)*/
-		  .click(function() {
-			chrome.tabs.create({
-			  'url' : bgPage.rcts[this.timestamp].url,
-			  'selected' : true
-			  }, function() { 
-			  // Tab opened: possible migration procedures
-			  });
-			delete bgPage.rcts[this.timestamp];
-			bgPage.storeRcts(bgPage.rcts);
-			for (var index in bgPage.rctTimestamps) if (bgPage.rctTimestamps[index] == this.timestamp) bgPage.rctTimestamps.splice(index, 1);
-			window.close();
-		  });
-		url[0].timestamp = timestamp;
-		
-		var titleTextDiv = $('<div>')
-		  .addClass('titleTextDiv')
-		  .text(tab.title)
-		  .appendTo(url);
-		
-		var img = $('<img>')
-		  .addClass('favicon')
-		  .attr({ src: tab.favIconUrl })
-		  .appendTo(url);
-		
-		url.appendTo(li);
-		li.appendTo(ul);
+  		counter++;
+  		if (counter > bgPage.maxPopupLength) {
+  		  break;
+  		}
+  		var timestamp = bgPage.rctTimestamps[index];
+  		var tab = bgPage.rcts[timestamp];
+  		var li = $('<li>');
+  		  
+  		var url = $('<a>')
+  		  .attr({ 
+    			href: '#',
+    			title: tab.url
+  		  })
+  		  /*.text(tab.title)*/
+  		  .click(function() {
+    			chrome.tabs.create({
+    			  'url' : bgPage.rcts[this.timestamp].url,
+    			  'selected' : true
+    			  }, function() { 
+    			  // Tab opened: possible migration procedures
+    			  });
+    			delete bgPage.rcts[this.timestamp];
+    			bgPage.storeRcts(bgPage.rcts);
+    			for (var index in bgPage.rctTimestamps) if (bgPage.rctTimestamps[index] == this.timestamp) bgPage.rctTimestamps.splice(index, 1);
+    			window.close();
+  		  });
+  		url[0].timestamp = timestamp;
+  		
+  		var titleTextDiv = $('<div>')
+  		  .addClass('titleTextDiv')
+  		  .text(tab.title)
+  		  .appendTo(url);
+  		
+  		var popup_favIconUrl = tab.favIconUrl;
+  		if(popup_favIconUrl === "" || popup_favIconUrl === undefined) {
+  		  popup_favIconUrl = "../images/default_favicon.png";
+  		}
+  		var img = $('<img>')
+  		  .addClass('favicon')
+  		  .attr({ src: popup_favIconUrl })
+  		  .appendTo(url);
+  		
+  		url.appendTo(li);
+  		li.appendTo(ul);
 	  }
 	  ul.appendTo('body');
 	}
