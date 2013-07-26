ae696cdd2cf7a36308fceb3942b1b7dea4ded225
Draško Kokić <drasko.kokic@quartett-mobile.de>
2013-07-26 13:57:09 +0200

    minor updates so that it works with Manifest v2

--- js/background.js
+++ js/background.js
@@ -1,4 +1,21 @@
+//
 // Scripts for the background.html
+//
+
+//------------------------------------------------------------------------------
+// Since Manifest v2 does not allow inline scripts and event handlers ...
+//------------------------------------------------------------------------------
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-3928511-2']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+window.addEventListener("load", main);
 
 //------------------------------------------------------------------------------
 // Global variables
