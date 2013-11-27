1b72363f137cee893029169ae0b84ef91031cf06
Draško Kokić <drasko.kokic@googlemail.com>
2013-11-27 04:38:46 +0100

    moved inline js code out of html

--- js/infonews.js
+++ js/infonews.js
@@ -1,7 +1,29 @@
+//
 // Scripts for the infonews.html
+//
 
 //------------------------------------------------------------------------------
-// Everything starts here
+// Since Manifest v2 does not allow inline scripts and event handlers ...
+//------------------------------------------------------------------------------
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-3928511-2']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+window.addEventListener("load", main);
+
+//------------------------------------------------------------------------------
+// Global variables.
+//------------------------------------------------------------------------------
+var bgPage = chrome.extension.getBackgroundPage();
+
+//------------------------------------------------------------------------------
+// Main method: Everything starts here
 //------------------------------------------------------------------------------
 function main() {
 	this.createHeader('Infos');
